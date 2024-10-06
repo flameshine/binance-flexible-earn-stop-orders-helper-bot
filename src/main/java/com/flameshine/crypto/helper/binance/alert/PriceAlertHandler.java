e530cfaf28cf355a29d9767b10097009accaee5e
Reichemann <excellenceissoon@gmail.com>
2024-10-06 14:42:53 +0200

    Whitespace changes

--- src/main/java/com/flameshine/crypto/helper/binance/alert/PriceAlertHandler.java
+++ src/main/java/com/flameshine/crypto/helper/binance/alert/PriceAlertHandler.java
@@ -3,8 +3,6 @@ package com.flameshine.crypto.helper.binance.alert;
 import com.flameshine.crypto.helper.binance.model.PriceAlert;
 
 public interface PriceAlertHandler {
-
     void handle(PriceAlert priceAlert);
-
     void remove(Long alertId);
 }
--- src/main/java/com/flameshine/crypto/helper/binance/config/ApiConfig.java
+++ src/main/java/com/flameshine/crypto/helper/binance/config/ApiConfig.java
@@ -4,8 +4,6 @@ import io.smallrye.config.ConfigMapping;
 
 @ConfigMapping(prefix = "binance")
 public interface ApiConfig {
-
     String streamUrl();
-
     String flexibleEarnUrl();
 }
